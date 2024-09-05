aa2520a3a3e754f997a6a76d0172ae480e107800
Richard Bowden <rb@rdbg.co.uk>
2024-09-06 00:03:34 +0800

    add NewWithSourceAndDatabaseInstance

--- migrate.go
+++ migrate.go
@@ -114,6 +114,18 @@ func New(sourceURL, databaseURL string) (*Migrate, error) {
 	return m, nil
 }
 
+func NewWithSourceAndDatabaseInstance(sourceName string, sourceInstance source.Driver, databaseName string, databaseInstance database.Driver) (*Migrate, error) {
+
+	m := newCommon()
+	m.sourceName = sourceName
+	m.sourceDrv = sourceInstance
+
+	m.databaseDrv = databaseInstance
+	m.databaseName = databaseName
+
+	return m, nil
+}
+
 // NewWithDatabaseInstance returns a new Migrate instance from a source URL
 // and an existing database instance. The source URL scheme is defined by each driver.
 // Use any string that can serve as an identifier during logging as databaseName.
